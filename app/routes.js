1cd53e086b8a39efbc0bbf8d7c27f81d7452a0e8
glancyea <glancyea@gmail.com>
2016-04-13 16:51:07 +0100

    Fix routes for version 1 journey

--- app/routes.js
+++ app/routes.js
@@ -24,7 +24,6 @@ router.use(function (req, res, next) {
 
 router.get('/', function (req, res) {
   res.render('index');
-
 });
 
 // Example routes
@@ -57,40 +56,40 @@ router.get('/examples/over-18', function (req, res) {
 // River permissions prototype version 1
 
 // Start
+
 // Have you called us to check the location of your activity?                   /version_1/start/check_location
 // ###  location_check_no = Contact the Environment Agency                      /version_1/contact/contact_ea
 // ###  location_check_yes = Add the exemptions you want to register            /version_1/exemptions/add_exemptions
-// Check your exemptions                                                        /version_1/exemptions/check_exemptions
-// Where will the exemption activity take place?                                /version_1/location/grid_reference_2
-// Who is legally responsible for the activity?                                 /version_1/user_type/user_type
-// ###  public = What’s the name of the local authority or public body?         /version_1/user_type/local_authority
-// What’s the postcode of the local authority or public body?                   /version_1/address/local_authority_postcode
-// What’s the address of the local authority or public body?                    /version_1/address/local_authority
-// Who should we contact about this activity?                                   /version_1/contact/main_contact_name
-// What’s the telephone number of the person we should contact?                 /version_1/contact/main_contact_telephone
-// What’s the email address of the person we should contact?                    /version_1/contact/main_contact_email
-// What’s the postcode of the person we should contact?                         /version_1/contact/main_contact_address
-// What’s the address of the person we should contact?                          /version_1/contact/main_contact_postcode
-// Would you like a copy of the registration email sent to someone else?        /version_1/contact/email_someone_else
-// Check your answers before completing this registration                       /version_1/end_registration/check_your_answers
-// Declaration                                                                  /version_1/end_registration/declaration
-// Registration complete                                                        /version_1/end_registration/confirmation
 
-
-// Route location to exemptions page
-
-  router.post('/version_1/location/activity_specific/bridge_grid_reference', function(req, res) {
+router.post('/version_1/start/check_location', function (req,res) {
+  if (req.body['location_check']==="location_check_no"){
+    res.redirect('/version_1/contact/contact_ea');
+  } else if (req.body['location_check']==="location_check_yes") {
     res.redirect('/version_1/exemptions/add_exemptions');
-  });
+  }
+});
 
-  router.post('/version_1/exemptions/add_exemptions', function(req, res) {
-    res.redirect('/version_1/exemptions/check_exemptions');
-  });
+// Add the exemptions you want to register            /version_1/exemptions/add_exemptions
 
-  router.post('/version_1/exemptions/check_exemptions', function(req, res) {
-    res.redirect('/version_1/user_type/user_type');
-  });
+router.post('/version_1/exemptions/add_exemptions', function(req, res) {
+  res.redirect('/version_1/exemptions/check_exemptions');
+});
 
+// Check your exemptions                                                        /version_1/exemptions/check_exemptions
+
+router.post('/version_1/exemptions/check_exemptions', function(req, res) {
+  res.redirect('/version_1/location/grid_reference_2');
+});
+
+// Where will the exemption activity take place?                                /version_1/location/grid_reference_2
+
+router.post('/version_1/location/grid_reference_2', function(req, res) {
+  res.redirect('/version_1/user_type/user_type');
+});
+
+// Who is legally responsible for the activity?                                 /version_1/user_type/user_type
+
+// ###  public = What’s the name of the local authority or public body?         /version_1/user_type/local_authority
 // Route to relevant business type journey
 
 router.post('/version_1/user_type/user_type', function (req,res) {
@@ -118,203 +117,78 @@ router.post('/version_1/user_type/user_type', function (req,res) {
 
 });
 
-// Route local authority and public body
+// What’s the name of the local authority or public body?         /version_1/user_type/local_authority
 
 router.post('/version_1/user_type/local_authority', function(req, res) {
   res.redirect('/version_1/address/local_authority_postcode');
 });
 
+// What’s the postcode of the local authority or public body?                   /version_1/address/local_authority_postcode
+
 router.post('/version_1/address/local_authority_postcode', function(req, res) {
   res.redirect('/version_1/address/local_authority');
 });
+
+// What’s the address of the local authority or public body?                    /version_1/address/local_authority
 
 router.post('/version_1/address/local_authority', function(req, res) {
   res.redirect('/version_1/contact/main_contact_name');
 });
 
+// Who should we contact about this activity?                                   /version_1/contact/main_contact_name
+
 router.post('/version_1/contact/main_contact_name', function(req, res) {
   res.redirect('/version_1/contact/main_contact_telephone');
 });
 
+// What’s the telephone number of the person we should contact?                 /version_1/contact/main_contact_telephone
+
 router.post('/version_1/contact/main_contact_telephone', function(req, res) {
   res.redirect('/version_1/contact/main_contact_email');
 });
+
+// What’s the email address of the person we should contact?                    /version_1/contact/main_contact_email
 
 router.post('/version_1/contact/main_contact_email', function(req, res) {
   res.redirect('/version_1/contact/main_contact_address');
 });
 
+// What’s the postcode of the person we should contact?                         /version_1/contact/main_contact_address
+
 router.post('/version_1/contact/main_contact_address', function(req, res) {
   res.redirect('/version_1/contact/main_contact_postcode');
 });
 
+// What’s the address of the person we should contact?                          /version_1/contact/main_contact_postcode
+
 router.post('/version_1/contact/main_contact_postcode', function(req, res) {
-  res.redirect('/version_1/contact/applicant_contact');
+  res.redirect('/version_1/contact/email_someone_else');
 });
 
-// Route limited company
+// Would you like a copy of the registration email sent to someone else?        /version_1/contact/email_someone_else
 
-router.post('/version_1/user_type/limited_company_number', function(req, res) {
-  res.redirect('/version_1/user_type/limited_company_name');
-});
-
-router.post('/version_1/user_type/limited_company_name', function(req, res) {
-  res.redirect('/version_1/address/limited_company_postcode');
-});
-
-router.post('/version_1/address/limited_company_postcode', function(req, res) {
-  res.redirect('/version_1/address/limited_company_address');
-});
-
-router.post('/version_1/address/limited_company_address', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_name');
-});
-
-router.post('/version_1/contact/main_contact_name', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_telephone');
-});
-
-router.post('/version_1/contact/main_contact_telephone', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_email');
-});
-
-router.post('/version_1/address/limited_company_address', function(req, res) {
-  res.redirect('/version_1/contact/applicant_contact');
-});
-
-// Route limited liablity company
-
-router.post('/version_1/user_type/limited_liability_number', function(req, res) {
-  res.redirect('/version_1/user_type/limited_liability_name');
-});
-
-router.post('/version_1/user_type/limited_liability_name', function(req, res) {
-  res.redirect('/version_1/address/limited_liability_postcode');
-});
-
-router.post('/version_1/address/limited_liability_postcode', function(req, res) {
-  res.redirect('/version_1/address/limited_liability_address');
-});
-
-router.post('/version_1/address/limited_liability_address', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_name');
-});
-
-router.post('/version_1/contact/main_contact_name', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_telephone');
-});
-
-router.post('/version_1/contact/main_contact_telephone', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_email');
-});
-
-router.post('/version_1/address/limited_liability_address', function(req, res) {
-  res.redirect('/version_1/contact/applicant_contact');
-});
-
-// Route individual
-
-router.post('/version_1/user_type/individual_name', function(req, res) {
-  res.redirect('/version_1/address/individual_postcode');
-});
-
-router.post('/version_1/address/individual_postcode', function(req, res) {
-  res.redirect('/version_1/address/individual_address');
-});
-
-router.post('/version_1/address/individual_address', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_name');
-});
-
-router.post('/version_1/contact/main_contact_name', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_telephone');
-});
-
-router.post('/version_1/contact/main_contact_telephone', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_email');
-});
-
-router.post('/version_1/address/individual_address', function(req, res) {
-  res.redirect('/version_1/contact/applicant_contact');
-});
-
-// Route partnership
-
-router.post('/version_1/user_type/partnership', function(req, res) {
-  res.redirect('/version_1/user_type/partnership_address');
-});
-
-router.post('/version_1/user_type/partnership_address', function(req, res) {
-  res.redirect('/version_1/user_type/partnership_details');
-});
-
-router.post('/version_1/user_type/partnership_details', function(req, res) {
-  res.redirect('/version_1/contact/applicant_contact');
-});
-
-// Route Others
-
-router.post('/version_1/user_type/other', function(req, res) {
-  res.redirect('/version_1/address/other_postcode');
-});
-
-router.post('/version_1/address/other_postcode', function(req, res) {
-  res.redirect('/version_1/address/other_address');
-});
-
-router.post('/version_1/address/other_address', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_name');
-});
-
-router.post('/version_1/contact/main_contact_name', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_telephone');
-});
-
-router.post('/version_1/contact/main_contact_telephone', function(req, res) {
-  res.redirect('/version_1/contact/main_contact_email');
-});
-
-router.post('/version_1/contact/main_contact_email', function(req, res) {
-  res.redirect('/version_1/contact/applicant_contact');
-});
-
-// Route contact details
-
-router.post('/version_1/contact/applicant_contact', function (req,res) {
-  if (req.body['radio-inline-group']==="contact_yes"){
-    res.redirect('/version_1/end_registration/check_your_answers_no_contact');
-  } else {
-    res.redirect('/version_1/contact/applicant_contact_name');
-  }
-});
-
-router.post('/version_1/contact/applicant_contact_name', function(req, res) {
-  res.redirect('/version_1/contact/applicant_contact_telephone');
-});
-
-router.post('/version_1/contact/applicant_contact_telephone', function(req, res) {
-  res.redirect('/version_1/contact/applicant_contact_email');
-});
-
-router.post('/version_1/contact/applicant_contact_email', function(req, res) {
+router.post('/version_1/contact/email_someone_else', function(req, res) {
   res.redirect('/version_1/end_registration/check_your_answers');
 });
 
-// Redirects end of check your answers and declaration
+// Check your answers before completing this registration                       /version_1/end_registration/check_your_answers
 
 router.post('/version_1/end_registration/check_your_answers', function(req, res) {
   res.redirect('/version_1/end_registration/declaration');
 });
 
+// Declaration                                                                  /version_1/end_registration/declaration
+
 router.post('/version_1/end_registration/declaration', function(req, res) {
   res.redirect('/version_1/end_registration/confirmation');
 });
 
-router.post('/version_1/end_registration/check_your_answers_no_contact', function(req, res) {
-  res.redirect('/version_1/end_registration/declaration'); });
+// Registration complete                                                        /version_1/end_registration/confirmation
 
 
-// DESIGN AREA ROUTING
+
+// DESIGN AREA ROUTING #############################################################################
+// #################################################################################################
 
 // Location
 
